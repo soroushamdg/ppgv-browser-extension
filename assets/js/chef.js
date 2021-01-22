2b24000eba951c51e78128a6ac1a38830280a99b
soroushamdg <soroushmpour@gmail.com>
2021-01-23 00:36:02 +0330

    - git migration

---
+++ assets/js/chef.js
@@ -0,0 +1,153 @@
+
+var url;
+var registered_gateways;
+
+var ssl_check;
+var db_check;
+
+function readregsfile(){
+    /// Start reading registered-gateways.rg file.
+    var request = new XMLHttpRequest();
+    request.open('GET', 'assets/js/registered-gateways.rg', true);
+    request.send(null);
+    request.onload = function() {
+        if (request.readyState === 4 && request.status === 200) {
+            var type = request.getResponseHeader('Content-Type');
+            if (type.indexOf("text") !== 1) {
+                registered_gateways = JSON.parse(request.responseText);
+                
+                if (url.hostname in registered_gateways["registered"]){
+                    db_check = true;
+                }else{
+                    db_check = false;
+                }
+
+                setDOM();
+            }
+        }
+    };
+}
+
+
+function getct(callback) { // gets current tab's URL.
+chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
+    url = tabs[0].url;
+    callback(url);
+});
+}
+
+function checkssl(url){ // checks if webpage is connected through SSL.
+    if (typeof(url) == "object"){
+        try{
+            ssl_check = true;
+            return url.protocol == "https:";
+        }catch{
+            ssl_check = false;
+            throw "Error in parsing URL to check SSL, Plase check again."
+        }
+    }else{
+        ssl_check = false;
+        throw "Check SSL input is not a URL, Please check again."
+    }
+}
+
+function checkurlindb(){ // checks if URL is already defined in database.
+    readregsfile();
+}
+
+
+function setprocessstat(stat){ // stat can be 1. secure 2. average 3. not secure and will change html.
+    switch (stat){
+        case "secure":
+            document.getElementById("process-state-heading").innerText = "✔ کاملا امن است";
+            document.getElementById("process-state-heading").style.color = "rgb(91,191,33)";
+            document.getElementById("process-image-image").setAttribute("src","/assets/img/secure-ok.svg?h=31ebcf89c96b160fa85bf147fede3629");
+            break;
+        case "average":
+            document.getElementById("process-state-heading").innerText = "⚠ درگاه نیمه امن است";
+            document.getElementById("process-state-heading").style.color = "rgb(255,147,56)";
+            document.getElementById("process-image-image").setAttribute("src","/assets/img/process-warning.svg?h=6290b380f5a28b196754c9875b2edcbe");
+            break;
+        case "insecure":
+            document.getElementById("process-state-heading").innerText = "❗ درگاه امن نیست";
+            document.getElementById("process-state-heading").style.color = "rgb(252,35,102)";
+            document.getElementById("process-image-image").setAttribute("src","/assets/img/process-insecure.svg?h=805eb3b642e692f547f3640db0c67ea5");
+            break;
+        default:
+            document.getElementById("process-state-heading").innerText = "خطا در بررسی!";
+            document.getElementById("process-state-heading").style.color = "rgb(142,73,5)";
+            document.getElementById("process-image-image").setAttribute("src","/assets/img/feeling-angry-animate.svg?h=88a0cbaf9c139a19d6ae52e9e471ca2d");
+            break;
+    }
+}
+
+function setgatewayinfo(gateway){ // input is gateway information object.
+    document.getElementById("gateway-icon-image").setAttribute("src","/assets/img/" + gateway["logo_path"]);
+    document.getElementById("gateway-name-heading").textContent = gateway["name_fa"];
+    document.getElementById("gateway-phone-heading").text = gateway["phone"];
+    document.getElementById("gateway-phone-heading").setAttribute("href","tel:"+gateway["phone"]);
+}
+
+function setsslchecklist(stat){
+    if (stat){
+        document.getElementById("ssl-checklist-icon-image").setAttribute("src","/assets/img/icons-tick-box.svg?h=61429b05e6097d99041b07b7005b0101");
+    }else{
+        document.getElementById("ssl-checklist-icon-image").setAttribute("src","/assets/img/icons-delete.svg?h=f7ba2fa718f6771d9a919dc09e83a9e5");
+    }
+}
+
+function setlookindbchecklist(stat){
+    if (stat){
+        document.getElementById("db-checlist-icon-image").setAttribute("src","/assets/img/icons-tick-box.svg?h=61429b05e6097d99041b07b7005b0101");
+    }else{
+        document.getElementById("db-checlist-icon-image").setAttribute("src","/assets/img/icons-delete.svg?h=f7ba2fa718f6771d9a919dc09e83a9e5");
+    }
+}
+
+
+function setDOM(){
+// set information on DOM
+if(ssl_check && db_check){
+    setprocessstat('secure');
+}else if(ssl_check || db_check){
+    setprocessstat('average');
+}else if (!ssl_check && !db_check){
+    setprocessstat('insecure');
+}else{
+    setprocessstat('no_info');
+}
+
+if (db_check){
+    setgatewayinfo(registered_gateways["registered"][url.hostname]);
+    setlookindbchecklist(true);
+}else{
+    setlookindbchecklist(false);
+ 
+}
+
+if (ssl_check){
+    setsslchecklist(true);
+}else{
+    setsslchecklist(false);
+
+}
+
+// unhide DOM elements.
+document.getElementById("check-box-card").removeAttribute('hidden');
+
+if (db_check){
+    document.getElementById("gateway-information-row").removeAttribute('hidden');
+}
+
+}
+
+getct(function(callback_url){ // Get URL
+
+
+    url = new URL(callback_url);
+
+    // check the url
+    checkssl(url);
+    checkurlindb();
+
+});
---
+++ assets/js/registered-gateways.rg
@@ -0,0 +1,101 @@
+{
+  "registered": {
+    "sep.shaparak.ir": {
+      "url": "sep.shaparak.ir",
+      "name_fa": "پرداخت الکترونیک سامان",
+      "name_en": "Saman Electeronic Payment",
+      "logo_path": "sep_ir.png",
+      "corporate_url": "https://sep.ir",
+      "phone": "+982184080"
+    },
+    "ecd.shaparak.ir": {
+      "url": "ecd.shaparak.ir",
+      "name_fa": "پرداخت الکترونیک کارت دماوند",
+      "name_en": "Damavand Electeronic Card Payment",
+      "logo_path": "damavand_ecp_ir.png",
+      "corporate_url": "http://ecd-co.ir",
+      "phone": "+982166737000"
+    },
+    "asan.shaparak.ir": {
+      "url": "asan.shaparak.ir",
+      "name_fa": "آسان پرداخت پرشین",
+      "name_en": "Asan Pardakht Persian",
+      "logo_path": "asan_pardakht_ir.png",
+      "corporate_url": "https://asanpardakht.ir/",
+      "phone": "+982183333"
+    },
+    "bpm.shaparak.ir": {
+      "url": "bpm.shaparak.ir",
+      "name_fa": "به پرداخت ملت",
+      "name_en": "Beh Pardakht Mellat",
+      "logo_path": "behpardakht_logo.svg",
+      "corporate_url": "http://behpardakht.com",
+      "phone": "+982127312731"
+    },
+    "pep.shaparak.ir": {
+      "url": "pep.shaparak.ir",
+      "name_fa": "پرداخت الکترونیک پاسارگاد",
+      "name_en": "Pasargad Electeronic Payment",
+      "logo_path": "pasargad_ep.png",
+      "corporate_url": "http://pep.co.ir",
+      "phone": "+982123505"
+    },
+    "pec.shaparak.ir": {
+      "url": "pec.shaparak.ir",
+      "name_fa": "تجارت الکترونیک پارسیان",
+      "name_en": "Parsian Electronic Commerce",
+      "logo_path": "pec_ir.png",
+      "corporate_url": "http://pec.ir",
+      "phone": "+982122661777"
+    },
+    "sadad.shaparak.ir": {
+      "url": "sadad.shaparak.ir",
+      "name_fa": "پرداخت الکترونیک سداد",
+      "name_en": "Sadad Electeronic Payment",
+      "logo_path": "sadad_ir.jpg",
+      "corporate_url": "http://sadadpsp.ir",
+      "phone": "+982122598009"
+    },
+    "fcp.shaparak.ir": {
+      "url": "fcp.shaparak.ir",
+      "name_fa": "فن آوا کارت",
+      "name_en": "Fan Ava Card",
+      "logo_path": "fanavacard_ir.png",
+      "corporate_url": "https://www.fanavacard.ir",
+      "phone": "+982143695000"
+    },
+    "sayan.shaparak.ir": {
+      "url": "sayan.shaparak.ir",
+      "name_fa": "سایان کارت",
+      "name_en": "Sayan Card",
+      "logo_path": "sayancard_ir.png",
+      "corporate_url": "http://sayancard.ir",
+      "phone": "+982123762376"
+    },
+    "ikc.shaparak.ir": {
+      "url": "ikc.shaparak.ir",
+      "name_fa": "کارت اعتباری ایران کیش",
+      "name_en": "Iran Kish Credit Card",
+      "logo_path": "irankish_ir.png",
+      "corporate_url": "http://irankish.com",
+      "phone": "+982185945501"
+    },
+    "pna.shaparak.ir": {
+      "url": "pna.shaparak.ir",
+      "name_fa": "پرداخت نوین آرین",
+      "name_en": "Novin Arian Payment",
+      "logo_path": "novinarainp_ir.png",
+      "corporate_url": "http://pna.co.ir",
+      "phone": "+982148320"
+    },
+    "mabna.shaparak.ir": {
+      "url": "mabna.shaparak.ir",
+      "name_fa": "پرداخت الکترونیک سپهر",
+      "name_en": "Sepehr Electeronic Payment",
+      "logo_path": "sepehrpay_ir.png",
+      "corporate_url": "http://www.sepehrpay.com",
+      "phone": "+982189396000"
+    }
+  },
+  "unregistered": {}
+}
